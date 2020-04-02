f01c1adb0531e188c47c19bd321f64e8bfba757f
Negin Sauermann <negin.sauermann@campbrain.com>
2020-04-02 14:11:41 -0400

    remove boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hi"/>
   </div>
 </template>
 
@@ -16,7 +15,7 @@ export default {
 }
 </script>
 
-<style>
+<style> 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
