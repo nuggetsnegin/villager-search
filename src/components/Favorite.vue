9b35d44183eed664aaa2152d277c79d2bc337a24
Negin Sauermann <negin.sauermann@campbrain.com>
2020-04-02 21:50:49 -0400

    binding ac data that andrew scrapped 😊

--- src/components/Favorite.vue
+++ src/components/Favorite.vue
@@ -2,7 +2,13 @@
   <div class="wrapper">
     <h1>Villagers</h1>
     <div v-bind:key="villager.id" v-for="villager in villagers">
-      <h3>{{ villager.name }}</h3>
+      <ul class="villager-card">
+        <li class="villager-name">{{ villager.name }}</li>
+        <li class="villager-image"><img v-bind:src="villager.image" /></li>
+        <li class="villager-species">{{ villager.species }}</li>
+        <li class="villager-personality">{{ villager.personality }}</li>
+        <li class="villager-birthday">{{ villager.birthday }}</li>
+      </ul>
     </div>
   </div>
 </template>
